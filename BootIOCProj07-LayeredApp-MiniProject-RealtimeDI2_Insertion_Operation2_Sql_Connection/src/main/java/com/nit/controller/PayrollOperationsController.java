4751f0e49523383ef90c9a3433bd5770f6761ac9
GANESH <GANESH@192.168.1.35>
2025-11-13 15:15:45 +0530

    Initial Commit

---
+++ BootIOCProj07-LayeredApp-MiniProject-RealtimeDI2_Insertion_Operation2_Sql_Connection/src/main/java/com/nit/controller/PayrollOperationsController.java
@@ -0,0 +1,32 @@
+package com.nit.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.nit.model.Employee;
+import com.nit.service.IEmployeeMgmtService;
+
+@Controller("controller")
+public class PayrollOperationsController 
+{
+	@Autowired
+	private  IEmployeeMgmtService  empService;
+	
+	public    List<Employee>   getEmployeesByDesgs(String desg1,String desg2,String desg3 ) throws Exception{
+		System.out.println("PayrollOperationsController.getEmployeesByDesgs()");
+		 //use service
+		List<Employee>  list=empService.fetchEmployeesByDesgs(desg1, desg2, desg3);
+		return list;
+	}
+	
+	public String processEmployee(Employee emp) throws Exception
+	{
+		//use service
+		String resultMsg = empService.registerEmployee(emp);
+		return resultMsg;
+	}
+
+	
+}
---
+++ BootIOCProj07-LayeredApp-MiniProject-RealtimeDI2_Insertion_Operation2_Sql_Connection/src/main/java/com/nit/dao/IEmployeeOracleDAOImpl.java
@@ -0,0 +1,106 @@
+package com.nit.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.nit.model.Employee;
+
+@Repository("empDAO")
+public class IEmployeeOracleDAOImpl implements IEmployeeDAO {
+	
+	@Autowired
+	private  DataSource  ds;
+	private   static  final  String  GET_EMPS_BY_DESGS="SELECT EMPNO,ENAME,JOB,SAL FROM EMP WHERE JOB IN (?,?,?) ORDER BY JOB";
+	private static final String INSERT_EMPLOYEE = "INSERT INTO EMP(EMPNO,ENAME,JOB,SAL) VALUES (EMPNO_SEQ1.NEXTVAL,?,?,?)";
+
+	@Override
+	public List<Employee> showEmployeesByDesgs(String desg1, String desg2, String desg3) throws Exception 
+	{
+		System.out.println("IEmployeeDAOImpl.showEmployeesByDesgs()");
+		List<Employee> list=null;
+		   // persistence logic using JDBC code
+		try( //get Poold jdbc con object
+				Connection con=ds.getConnection();
+				//create PreparedSTatement object
+				PreparedStatement  ps=con.prepareStatement(GET_EMPS_BY_DESGS);
+				){
+			  //set  values to Query params
+			  ps.setString(1, desg1);
+			  ps.setString(2, desg2);
+			  ps.setString(3, desg3);
+			  try(  //execute the SQL Query
+					    ResultSet rs=ps.executeQuery();  ){
+				  //copy  ResultSEt object records  List of Java bean class objs
+				    list=new ArrayList();
+				  while(rs.next()) {
+					   //copy  each record of ResultSEt obj to Employee class obj
+					  Employee emp=new Employee();
+					  emp.setEmpNo(rs.getInt(1));
+					  emp.setEname(rs.getString(2));
+					  emp.setJob(rs.getString(3));
+					  emp.setSal(rs.getFloat(4));
+					  // add each Model class obj to List Collection
+					  list.add(emp);
+				  }//while
+				  
+			  }//try2
+			}//try1
+		catch(SQLException se) {
+			throw se; //Exception rethrowing
+		}
+		catch(Exception e) {
+			throw e;  //Exception rethrowing
+		}
+		return list;
+	}
+		
+		
+		@Override
+		public int insertEmployee(Employee emp) throws Exception
+		{
+			int result = 0;
+			try(//get pooled connection)
+					
+					Connection con = ds.getConnection();
+					
+					//create preparedstatement obj having the pre-compiled sql query
+					PreparedStatement ps = con.prepareStatement(INSERT_EMPLOYEE);
+					)
+			{
+				//set values to query params
+				
+				ps.setString(1,emp.getEname());
+				ps.setString(2,emp.getJob());
+				ps.setFloat(3, emp.getSal());
+				
+				
+				
+				//execute the sql query
+				result = ps.executeUpdate();
+				
+			}
+			
+			catch(SQLException se)
+			{
+				se.printStackTrace();
+				throw se;
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				throw e;
+			}
+			return result;
+				
+		}//method	}
+
+}
